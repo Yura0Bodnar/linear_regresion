553a0a0c4f5f519e8be200a3af07f650a1e19ce0
Yura0Bodnar <yuriy575721@gmail.com>
2024-05-19 20:50:37 +0300

    added gradient descent, and comparison of results with the embedded function

--- main.py
+++ main.py
@@ -1,47 +1,61 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
-# Завантаження даних
-data = pd.read_csv('adm_data.csv')
 
-# Попередня обробка
-# Видалення рядків, де відсутні дані по GRE Score або TOEFL Score
-data = data.dropna(subset=['GRE Score', 'TOEFL Score'])
+def mserror(X, w, y):
+    y_pred = X.dot(w)
+    return np.sum((y - y_pred) ** 2) / len(y_pred)
 
-# Отримання векторів для незалежних та залежної змінних
-X = data[['GRE Score']]  # Незалежна змінна
-y = data['TOEFL Score']  # Залежна змінна
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+def gradient_mse(X, w, y, lambda_l1):
+    y_pred = X.dot(w)
+    grad = 2/len(X) * X.T.dot(y_pred - y)
+    # Додавання градієнта штрафу L1
+    #l1_grad = lambda_l1 * np.sign(w)
+    return grad #+ l1_grad
 
-# Створення моделі лінійної регресії
-model = LinearRegression()
 
-# Тренування моделі
-model.fit(X_train, y_train)
+def main():
+    data = pd.read_csv('adm_data.csv')
+    X = data[['GRE Score', 'TOEFL Score', 'University Rating', 'SOP', 'LOR ', 'CGPA', 'Research']].values
+    y = data['Chance of Admit '].values
+    X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+    y = (y - np.mean(y)) / np.std(y)
+    X = np.hstack([np.ones((X.shape[0], 1)), X])
 
-# Прогнозування
-y_pred = model.predict(X_test)
+    # Вбудована модель
+    b_model = LinearRegression()
+    b_model.fit(X, y)
+    y_pred_b = b_model.predict(X)
+    mse_b = mean_squared_error(y, y_pred_b)
+    print('Sklearn MSE:', mse_b)
 
-# Оцінка моделі
-mse = mean_squared_error(y_test, y_pred)
-res = model.score(X_train, y_train)
+    # lambda_l1 = 0.01  # Регуляризаційний параметр L1
+    w = np.zeros(X.shape[1])
+    learning_rate = 0.01
+    n_iterations = 2500
+    eps = 1e-6
+    mse = 0
 
-print('Mean Squared Error:', mse)
-print("Efficiency: ", res)
+    for i in range(n_iterations):
+        grad = gradient_mse(X, w, y, None)
+        new_w = w - learning_rate * grad
+        mse = mserror(X, new_w, y)
 
-# Коефіцієнти моделі
-print('Slope:', model.coef_[0])
-print('Intercept:', model.intercept_)
+        if i % 100 == 0:
+            print(f"Iteration {i}: MSE = {mse}")
 
-# Візуалізація результатів
-plt.scatter(X_test, y_test, color='black')  # Справжні значення для тестової вибірки
-plt.plot(X_test, y_pred, color='blue', linewidth=3)  # Лінія прогнозу
-plt.xlabel('GRE Score')
-plt.ylabel('TOEFL Score')
-plt.title('Linear Regression Analysis')
-plt.show()
+        if np.linalg.norm(new_w - w, ord=2) < eps:
+            print('Convergence reached after', i+1, 'iterations.')
+            break
+        w = new_w
+
+    print('Custom model coefficients:', w)
+    print('Custom MSE:', mse)
+
+
+if __name__ == '__main__':
+    main()
