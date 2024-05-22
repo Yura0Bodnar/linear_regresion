17b93bf411a2fa56fec87bf935515cbb521dbbb8
Yura0Bodnar <yuriy575721@gmail.com>
2024-05-22 18:08:37 +0300

    compared results

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import pandas as pd
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import time
 import matplotlib.pyplot as plt
 
-lambda_l1 = 0.001  # Регуляризаційний параметр L1
+lambda_l1 = 0.01  # Регуляризаційний параметр L1
 lambda_l2 = 0.01  # Регуляризаційний параметр L2
-learning_rate = 0.01
+learning_rate = 0.001
 n_iterations = 10000
-eps = 1e-5
+eps = 1e-4
 
 
 def compute_r_squared(y, y_pred):
@@ -46,12 +47,23 @@ def gradient_mse_l2(X, w, y, lambda_l2):
     return grad + l2_grad
 
 
+def gradient_mse_all(X, w, y, lambda_l1, lambda_l2):
+    y_pred = X.dot(w)
+    grad = 2/len(X) * X.T.dot(y_pred - y)
+    # Додавання градієнтів штрафів L1 і L2
+    l1_grad = lambda_l1 * np.sign(w)
+    l2_grad = 2 * lambda_l2 * w
+    return grad + l2_grad + l1_grad
+
+
 def solution(X_train, y_train, X_test, y_test, ind):
     mse = 0
     w = np.zeros(X_train.shape[1])
     for i in range(n_iterations):
         if ind == "l1":
             grad = gradient_mse_l1(X_train, w, y_train, lambda_l1)
+        elif ind == "all":
+            grad = gradient_mse_all(X_train, w, y_train, lambda_l1, lambda_l2)
         elif ind == "l2":
             grad = gradient_mse_l2(X_train, w, y_train, lambda_l2)
         else:
@@ -67,12 +79,12 @@ def solution(X_train, y_train, X_test, y_test, ind):
             break
         w = new_w
 
-    y_pred_train = X_train.dot(w)
-    r_squared_train = compute_r_squared(y_train, y_pred_train)
+    y_pred_test = np.dot(X_test, w)
+    r_squared = compute_r_squared(y_test, y_pred_test)
 
     print('Custom model coefficients:', w)
     print('Custom MSE train:', mse)
-    print('Train R^2 Score:', r_squared_train)
+    print('R^2 Score:', r_squared)
     print('\n')
 
 
@@ -90,12 +102,10 @@ def main():
     # Вбудована модель
     b_model = LinearRegression()
     b_model.fit(X_train, y_train)
-    y_pred_b_test = b_model.predict(X_test)
-    y_pred_b_train = b_model.predict(X_train)
-    mse_b_test = mean_squared_error(y_test, y_pred_b_test)
-    mse_b_train = mean_squared_error(y_train, y_pred_b_train)
-    print('Sklearn MSE test:', mse_b_test)
-    print('Sklearn MSE train:', mse_b_train)
+    y_pred_b = b_model.predict(X_train)
+    mse_b = mean_squared_error(y_train, y_pred_b)
+
+    print('Sklearn MSE:', mse_b)
 
     r2_train = b_model.score(X_train, y_train)  # Розрахунок коефіцієнта детермінації R^2
     r2_test = b_model.score(X_test, y_test)
@@ -107,13 +117,24 @@ def main():
     print('\n\n')
 
     print("Градієнтний спуск:")
+    start_time = time.time()
     solution(X_train, y_train, X_test, y_test, None)
+    print("Time elapsed: {:.2f} seconds".format(time.time() - start_time))
 
     print("Градієнтний спуск з L1:")
+    start_time = time.time()
     solution(X_train, y_train, X_test, y_test, ind="l1")
+    print("Time elapsed: {:.2f} seconds".format(time.time() - start_time))
 
     print("Градієнтний спуск з L2:")
+    start_time = time.time()
     solution(X_train, y_train, X_test, y_test, ind="l2")
+    print("Time elapsed: {:.2f} seconds".format(time.time() - start_time))
+
+    print("Градієнтний спуск з L1 та L2:")
+    start_time = time.time()
+    solution(X_train, y_train, X_test, y_test, ind="all")
+    print("Time elapsed: {:.2f} seconds".format(time.time() - start_time))
 
 
 if __name__ == '__main__':
